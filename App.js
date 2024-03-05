5aeb3131c316da46b5f77c50b45577dcf929fc7e
alyrute <117280585+alyrute@users.noreply.github.com>
2024-03-05 09:53:02 +0100

    Primer Commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('../AppGatitosAPI/assets/header.png')} // Ruta de la imagen de la cabecera
+        style={styles.headerImage}
+      />
+      <Text style={styles.text}>Contenido de la pantalla principal</Text>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerImage: {
+    width: '100%',
+    height: 150, // Ajusta la altura según tus necesidades
+    resizeMode: 'cover',
+  },
+  text: {
+    fontSize: 20,
+  },
 });
+
+export default HomeScreen;
